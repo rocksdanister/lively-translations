2fe803379ea1a6837ed2bcc4f09197d32277099d
Dani John <rocksdanister@gmail.com>
2020-08-23 00:26:13 +0530

    swedish translation + tool
    - swedish translation added
    - new tool to update old translation file with missing strings.

--- Tools/LivelyResxUpdater/LivelyResxUpdater/Program.cs
+++ Tools/LivelyResxUpdater/LivelyResxUpdater/Program.cs
@@ -3,19 +3,55 @@ using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 
-/// <summary>
-/// Program to convert Lively v0.9 or below language resx to v1.0
-/// All xml files need the metadata and comments removed (except root.)
-/// map file: "name" is the v1.0 keyname, and value is the v0.9 keyname.
-/// newf file: New v1.0 resource file.
-/// old file: v0.9 or below resource file.
-/// final.resx: final output, convertion to Unescaped is needed ie replace &lt with < etc.
-/// </summary>
 namespace LivelyResxUpdater
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            //UpdateResX();
+        }
+
+        /// <summary>
+        /// Fill the base xml with translated text, leaving the missing translation with base one.
+        /// All xml files need the metadata and comments removed (except root.)
+        /// newf file: Latest translation file (base)
+        /// old file: translated file with missing values.
+        /// final.resx: final output, convertion to Unescaped is needed ie replace &lt with < etc.
+        /// </summary>
+        public static void UpdateResX()
+        {
+            var old = new XmlDocument();
+            old.Load("xml\\Resources.sv-SE.resx");
+
+            var newf = new XmlDocument();
+            newf.Load("xml\\Resources.resx");
+
+            foreach (XmlNode oNode in old.DocumentElement.ChildNodes)
+            {
+                var oName = oNode.Attributes["name"]?.InnerText;
+                foreach (XmlNode nNode in newf.DocumentElement.ChildNodes)
+                {
+                    var nName = nNode.Attributes["name"]?.InnerText;
+                    if (nName == oName)
+                    {
+                        nNode.InnerText = oNode.InnerXml;
+                        break;
+                    }
+                }
+            }
+            newf.Save("xml\\final.resx");
+        }
+
+        /// <summary>
+        /// Convert Lively v0.9 or below language resx to v1.0
+        /// All xml files need the metadata and comments removed (except root.)
+        /// map file: "name" is the v1.0 keyname, and value is the v0.9 keyname.
+        /// newf file: New v1.0 resource file.
+        /// old file: v0.9 or below resource file.
+        /// final.resx: final output, convertion to Unescaped is needed ie replace &lt with < etc.
+        /// </summary>
+        public static void LegacyToNewResx()
         {
             var map = new XmlDocument();
             map.Load("xml\\mapping.xml");
@@ -33,12 +69,12 @@ namespace LivelyResxUpdater
                 foreach (XmlNode oNode in old.DocumentElement.ChildNodes)
                 {
                     var oName = oNode.Attributes["name"]?.InnerText;
-                    if(oName == mValue)
+                    if (oName == mValue)
                     {
                         foreach (XmlNode nNode in newf.DocumentElement.ChildNodes)
                         {
                             var nName = nNode.Attributes["name"]?.InnerText;
-                            if(nName == mName)
+                            if (nName == mName)
                             {
                                 nNode.InnerText = oNode.InnerXml;
                                 break;
